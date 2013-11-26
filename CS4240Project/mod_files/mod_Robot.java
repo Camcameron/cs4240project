b69d60c15e426534101a74dba3fdac6552d7ac45
csg3mf <csg3mf>
2013-11-25 23:20:42 -0500

    small change to mod_Robot

--- CS4240Project/mod_files/mod_Robot.java
+++ CS4240Project/mod_files/mod_Robot.java
@@ -19,7 +19,7 @@ public class mod_Robot extends BaseMod {
 	@Override
 	public void load() {
 		ModLoader.registerEntityID(EntityRobot.class, "Robot", 100);//registers the mobs name and id
-		ModLoader.addSpawn("Robot", 15, -5, 1, EnumCreatureType.monster);//makes the mob spawn in game
+		ModLoader.addSpawn("Robot", 15, -5, 1, EnumCreatureType.creature);//makes the mob spawn in game
 		ModLoader.addLocalization("entity.Robot.name", "Robot");//adds Mob name on the spawn egg
 		//creates the spawn egg, and changes color of egg
 		EntityList.entityEggs.put(Integer.valueOf(70), new EntityEggInfo(70, 894731, (new Color(21, 15, 6)).getRGB()));
