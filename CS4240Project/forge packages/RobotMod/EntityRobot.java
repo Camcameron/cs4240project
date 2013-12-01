fed648294f2a885a82ec7db9f673368d7d9a06c3
Camcameron <cpn2pf@virginia.edu>
2013-11-30 23:54:10 -0500

    Added strategy class along with two basic strategies Currently just alternating between them

--- CS4240Project/forge packages/RobotMod/EntityRobot.java
+++ CS4240Project/forge packages/RobotMod/EntityRobot.java
@@ -4,28 +4,55 @@ import javax.swing.text.html.parser.Entity;
 
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EntityLivingData;
 import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIBreakDoor;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public class EntityRobot extends EntityMob// extend this to make mob
 												// hostile
 {
+	EntityAction action;
+	boolean removeLater;
+	
 	public EntityRobot(World par1World) {
 		super(par1World);
+		removeLater = true;
 		this.experienceValue = 20;
-		this.tasks.addTask(1, new EntityAILookIdle(this));
+		//this.tasks.addTask(1, new EntityAILookIdle(this));
+		this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true)); this.tasks.addTask(0, new EntityAISwimming(this));
+        //this.tasks.addTask(1, new EntityAIBreakDoor(this));
+        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        this.tasks.addTask(3, new EntityAIAttackOnCollide(this, EntityVillager.class, 1.0D, true));
+        this.tasks.addTask(4, new EntityAIMoveTowardsRestriction(this, 1.0D));
+       // this.tasks.addTask(5, new EntityAIMoveThroughVillage(this, 1.0D, false));
+        this.tasks.addTask(6, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, false));
 		//this.texture = "";
 		//texture is set in RenderRobot.java
 		//this.texture = "/mob/RobotTexture.png";
@@ -33,7 +60,50 @@ public class EntityRobot extends EntityMob// extend this to make mob
 		//
 	}
 	
+	protected void applyEntityAttributes()
+    {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.followRange).setAttribute(40.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setAttribute(0.23000000417232513D);
+        this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setAttribute(3.0D);
+    }
+	
+	////////////////////
+	///Action classes///
+	public interface EntityAction{
+		public void performAction(EntityRobot robot);
+	}
+	
+	public class JumpAction implements EntityAction{
+		public void performAction(EntityRobot robot) {
+			robot.jump();	
+		}
+		
+	}
+	
+	public class AttackAction implements EntityAction{
+		public void performAction(EntityRobot robot) {
+			robot.setFire(1);	
+		}
+	}
+	
+	public void setAction(EntityAction action){
+		this.action = action;
+	}
+	///             ///
+	///////////////////
+	
 	public void onLivingUpdate() {
+//		this.jump();
+		if(removeLater == true){
+			this.setAction(new AttackAction());
+			removeLater = false;
+		}
+		else{
+			this.setAction(new JumpAction());
+			removeLater = true;
+		}
+	this.action.performAction(this);
 		super.onLivingUpdate();
 	}
 	
