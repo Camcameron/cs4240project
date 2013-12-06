1e4fd4ea213ade5ba4fcf2b487179d73aa8c3d7e
Camcameron <cpn2pf@virginia.edu>
2013-12-05 22:13:22 -0500

    Fixed crash errors. Still problems if more than one block exists. Be warned that switching ai behaviour takes a few seconds.

--- CS4240Project/forge packages/RobotMod/BlockControllerTileEntity.java
+++ CS4240Project/forge packages/RobotMod/BlockControllerTileEntity.java
@@ -123,4 +123,7 @@ public class BlockControllerTileEntity extends TileEntity {
 			//setState(IdleState);
 		}		
 	};
+	
+
+	
 }
--- CS4240Project/forge packages/RobotMod/RobotMod.java
+++ CS4240Project/forge packages/RobotMod/RobotMod.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityEggInfo;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.src.ModLoader;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.client.registry.RenderingRegistry;
@@ -113,6 +114,7 @@ public class RobotMod {
 		LanguageRegistry.instance().addStringLocalization("block.Controller.name", "en_US",  "Controller");
         MinecraftForge.setBlockHarvestLevel(controller, "shovel", 0);
         GameRegistry.registerBlock(controller, "Controller");
+        ModLoader.registerTileEntity(BlockControllerTileEntity.class, "BlockController");
 	}
 
 	@EventHandler
